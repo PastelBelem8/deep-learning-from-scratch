2ccd65cb0fab505f7c0d92c231768d433dd7551c
PastelBelem8 <catarina.belem@ist.utl.pt>
2020-03-21 17:02:52 +0000

    Update forward pass; Implement back prop

--- back_prop.py
+++ back_prop.py
@@ -9,37 +9,44 @@ def derivative_relu(x):
     return np.array([int(x > 0)])
 
 
-def derivative_relu(x): 
-    return x if x > 0 else 0
-
-
-def se(y_true: float, y_pred: float):
+def se(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     """Computes the Squared Error"""
-    return (y_true - y_pred) ** 2
+    return np.sum((y_true - y_pred) ** 2)
+
+
+def derivative_se(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray: 
+    """Computes the partial derivatives of the SE loss function."""
+    return -2 * (y_true - y_pred) 
 
 
 loss_funcs = {
     "se": se,
 }
 
+loss_funcs_derivatives = {
+    "se": derivative_se,        
+}
+
 activation_funcs = {
     "relu": relu,
 }
 
-derivative_funcs = {
+activation_funcs_derivatives = {
     "relu": derivative_relu, 
 }
 
 weights_initialization = {
     "zeros": np.zeros,
     "random": np.random.rand, 
+    "ones": np.ones,
 }
 
 # ----------------------------------------------------------------------
 #                               Main Methods  
 # ----------------------------------------------------------------------
 def forward_propagation(
-    sample: np.ndarray, 
+    X: np.ndarray, 
+    y_true: np.ndarray,
     Ws: np.ndarray, 
     activations: tuple, 
     loss_f: callable
@@ -53,8 +60,6 @@ def forward_propagation(
 
     Parameters
     ----------
-    sample: numpy.ndarray
-        Vector with the input values to start the forward pass from. 
     Ws: Iterable[numpy.ndarray]
         Iterable with the weight matrices to apply in the pre-activation
         computation. 
@@ -71,73 +76,112 @@ def forward_propagation(
         metadata accumulated during the forward pass, including the local 
         derivatives computed at each neuron.
     """
-    X, y_true = sample[features_pos], sample[label_pos]
-
     Zs = [X]
-    df_dZs, df_dWs = [], []
+    df_dZins, dZin_dZs, dZin_dWs = [], [], []
     activation_fs = list(map(lambda name: activation_funcs[name], activations))
-    derivative_fs = list(map(lambda name: derivative_funcs[name], activations))
+    derivative_fs = list(map(lambda name: activation_funcs_derivatives[name], activations))
 
     for i, W in enumerate(Ws): 
         activation_func = activations[i]
-
+        print(f"\n <<< Starting FP for layer {i}>>> \n")
+        print("W:", W.shape, "prev Z:", Zs[i].shape)
         # Pre-activation function
-        Z_in = W * Zs[i]
+        Z_in = W @ Zs[i]
+        print("After pre-activation (Z_in):", Z_in.shape)
         # Activation Function
         Z = np.apply_along_axis(activation_fs[i], 1, Z_in)
-        # Local derivatives
-        df_dZ_in = np.apply_along_axis(derivative_fs[i], 1, Z)
-        df_dZ = df_dZ_in * W
-        df_dW = df_dZ_in * Zs[i]
-
         Zs.append(Z)
-        # Local derivative with the contribution of each layer to to the 
-        # output of the following layer
-        df_dZs.append(df_dZ)
-        # Local derivative with the contribution of the weights to the 
-        # output of the following layer
-        df_dWs.append(df_dW)
 
+        # Local derivatives
+        print("Computing local derivatives...")
+        df_dZin = np.apply_along_axis(derivative_fs[i], 1, Z) 
+        df_dZins.append(df_dZin)
+
+        dZin_dZ = W.T
+        dZin_dZs.append(dZin_dZ)
+
+        dZin_dW = Zs[i].T
+        dZin_dWs.append(dZin_dW)
+
+    print("y_true:", y_true, "y_pred:", Zs[-1])
     loss = loss_funcs[loss_f](y_true, Zs[-1])
 
     metadata = {
         # Outputs
         "Zs": Zs, 
         # Local derivatives
-        "df_dZs": df_dZs,
-        "df_dWs": df_dWs, 
+        "df_dZins": df_dZins,
+        "dZin_dZs": dZin_dZs,
+        "dZin_dWs": dZin_dWs, 
         # Loss
-        "loss": loss,
+        "loss": loss, 
     }
-    return loss, metadata
+    return Zs[-1], loss, metadata
 
 
-def backward_propagation(error: np.ndarray, partial_ds: dict) -> np.ndarray: 
+def backward_propagation(y_true: np.ndarray, y_pred: np.ndarray, loss_func: str, metadata: dict) -> list: 
     """Propagates the errors backwards from the current layer `l` to the 
     previous layer `l-1`, using the chain rule to determine the contribution 
     of the components involved in computing the value of `l` to the total error. 
 
     Parameters
     ----------
-    error : numpy.ndarray
+    loss_func : str
         Represents the error contribution of the current layer `l` to the error. 
-    partial_ds: dict
-        Local partial derivatives involved in the forward pass from `l-1` to `l`.
+    metadata: dict
+        Information collected during the forward pass, which consists in the y_pred, 
+        y_true, the local partial derivatives involved in the forward pass and the 
+        output of each layer.
 
     Returns
     -------
-    dict    
-        The partial contributions of each component to the error. 
+    list[np.array]
+        The updates to apply to each Weight matrix. 
     """
-    pass
+    derivative_fs = loss_funcs_derivatives[loss_func]
+    dloss_df = derivative_fs(y_true, y_pred)
+
+    # Local derivatives
+    df_dZins = metadata["df_dZins"] 
+    dZin_dZs = metadata["dZin_dZs"]
+    dZin_dWs = metadata["dZin_dWs"]
+
+    Ws_updates = []
+    n_updates = len(df_dZins) - 1
+
+    for i in range(n_updates, -1, -1):
+        print(f"\n <<< Starting BP for layer {i}>>> \n")
+        df_dZin = df_dZins[i]
+        dZin_dZ = dZin_dZs[i]
+        dZin_dW = dZin_dWs[i]
+
+        print(dloss_df.shape, ".", df_dZin.shape)
+        dloss_dZin = np.multiply(dloss_df, df_dZin)
+        print("Propagating the error to the weights at layer:", i+1)
+        print(f"Local derivative\n\tdErr_dw () = dErr_dZ {dloss_dZin.shape} . Zi-1.T {dZin_dW.shape}")
+        dloss_dW = dloss_dZin @ dZin_dW
+        Ws_updates.append(dloss_dW)
+
+        # Propagate the gradient for the other layer
+        print("Propagating the gradient to the previous layer:", i)
+        print(f"Local derivative\n\tdf_dZ(i-1) () = W.T {dZin_dZ.shape} . dloss_dZin {dloss_dZin.shape}")
+        dloss_df = dZin_dZ @ dloss_dZin
+
+    return reversed(Ws_updates)
     
 
-def train_NN(data: np.ndarray, architecture: tuple, activations: tuple, loss_func: str, weights_init: str = "random") -> dict:
+def update(*args):
+    pass
+
+def train_NN(X: np.ndarray, y: np.ndarray, architecture: tuple, activations: tuple, loss_func: str, weights_init: str = "random") -> dict:
     """Trains a neural network with ``architecture`` and `activation_funcs`.
 
     Parameters
     ----------
-    data: numpy.ndarray
+    X: numpy.ndarray
+        Data to fit the neural network. 
+
+    y: numpy.ndarray
         Data to fit the neural network. 
 
     architecture: Iterable[int]
@@ -161,28 +205,51 @@ def train_NN(data: np.ndarray, architecture: tuple, activations: tuple, loss_fun
         Ws = []
         weight_init_f = weights_initialization[weights_init]
 
-        for i, layer in enumerate(architecture[1:]): 
-            prev_layer = architecture[i-1]
+        for i in range(1, len(architecture)): 
+            prev_layer, layer = architecture[i-1], architecture[i]
 
             W_size = (layer, prev_layer)
-            W = weight_init_f(*W_size)
+            W = weight_init_f(W_size)
             Ws.append(W)
 
         return Ws
 
     n_samples, _ = data.shape
     Ws = init_weights()
-
+    
+    # ---------------------------
+    # Compute batch update
+    # ---------------------------
+    updates = []
+    print("Processing NN with architecture:", architecture)
+    print("Processing", n_samples, "samples.")
     for i in range(n_samples):
-        sample = data[i, :]
-        loss, metadata = forward_propagation(sample, Ws, activations, loss_func)
+        x = X[i, :]
+        x = x.reshape(len(x),1)
+        
+        y_true = y[i, :]
+        y_true = y_true.reshape(len(y_true), 1)
 
-    print(metadata)
+        print("Sample.X:", X.shape, "Sample.y:", y_true.shape)
+        y_pred, loss, metadata = forward_propagation(x, y_true, Ws, activations, loss_func)
+        
+        print("\n\n [END FP] \n\n")
+        # Compute the backward_pass right away
+        Ws_updates = backward_propagation(y_true, y_pred, loss_func, metadata) 
+        Ws_sizes = map(lambda W: W.shape, Ws)
+        Ws_updates_sizes = map(lambda W: W.shape, Ws_updates)
+
+        print(list(Ws_sizes), "vs", list(Ws_updates_sizes))
+        # updates += Ws_updates
+    Ws = update(Ws, updates, n_samples)
 
 
-data = np.array([[1, 1], [1, 0], [0, 1], [0, 0]])
+data = np.array([[1, 1, 0, 1], [1, 0, 1, 2], [0, 1, 1, 3], [0, 0, 0, 4]])
 
-label_pos = 1
-features_pos = [0]
+n_features = 2
+n_labels = 2
+X = data[:, 0:n_features]
+y = data[:, n_features:]
 
-train_NN(data, (1, 2, 1), ("relu", "relu"), "se")
+print("X:", X.shape, "y:", y.shape)
+train_NN(X, y, (2, 5, 3, n_labels), ("relu", "relu", "relu", "relu"), "se", "ones")
